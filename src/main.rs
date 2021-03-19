af89ccb57d3b3b514647b99aaa78c23f221b2736
sidit77 <sidit77@gmail.com>
2021-03-19 22:39:47 +0100

    Added some basic texture support

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,7 @@ impl Camera {
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct Vertex {
     position: glam::Vec2,
-    color: glam::Vec3,
+    tex_coords: glam::Vec2,
 }
 
 struct JumpAndRun {
@@ -57,6 +57,7 @@ struct JumpAndRun {
     camera: Camera,
     uniform_buffer: wgpu::Buffer,
     uniform_bind_group: wgpu::BindGroup,
+    diffuse_bind_group: wgpu::BindGroup,
 }
 
 impl Game for JumpAndRun {
@@ -106,10 +107,115 @@ impl Game for JumpAndRun {
             label: Some("Uniform Bind Group"),
         });
 
+        let diffuse_bytes = include_bytes!("happy-tree.png");
+        let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
+        let diffuse_rgba = diffuse_image.as_rgba8().unwrap();
+
+        use image::GenericImageView;
+        let dimensions = diffuse_image.dimensions();
+
+        let texture_size = wgpu::Extent3d {
+            width: dimensions.0,
+            height: dimensions.1,
+            depth: 1,
+        };
+        let diffuse_texture = display.device.create_texture(
+            &wgpu::TextureDescriptor {
+                // All textures are stored as 3D, we represent our 2D texture
+                // by setting depth to 1.
+                size: texture_size,
+                mip_level_count: 1, // We'll talk about this a little later
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                // SAMPLED tells wgpu that we want to use this texture in shaders
+                // COPY_DST means that we want to copy data to this texture
+                usage: wgpu::TextureUsage::SAMPLED | wgpu::TextureUsage::COPY_DST,
+                label: Some("diffuse_texture"),
+            }
+        );
+
+        display.queue.write_texture(
+            // Tells wgpu where to copy the pixel data
+            wgpu::TextureCopyView {
+                texture: &diffuse_texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+            },
+            // The actual pixel data
+            diffuse_rgba,
+            // The layout of the texture
+            wgpu::TextureDataLayout {
+                offset: 0,
+                bytes_per_row: 4 * dimensions.0,
+                rows_per_image: dimensions.1,
+            },
+            texture_size,
+        );
+
+        let diffuse_texture_view = diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let diffuse_sampler = display.device.create_sampler(&wgpu::SamplerDescriptor {
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Nearest,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            ..Default::default()
+        });
+
+        let texture_bind_group_layout = display.device.create_bind_group_layout(
+            &wgpu::BindGroupLayoutDescriptor {
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStage::FRAGMENT,
+                        ty: wgpu::BindingType::Texture {
+                            multisampled: false,
+                            view_dimension: wgpu::TextureViewDimension::D2,
+                            sample_type: wgpu::TextureSampleType::Float { filterable: false },
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStage::FRAGMENT,
+                        ty: wgpu::BindingType::Sampler {
+                            comparison: false,
+                            filtering: true,
+                        },
+                        count: None,
+                    },
+                ],
+                label: Some("texture_bind_group_layout"),
+            }
+        );
+
+        let diffuse_bind_group = display.device.create_bind_group(
+            &wgpu::BindGroupDescriptor {
+                layout: &texture_bind_group_layout,
+                entries: &[
+                    wgpu::BindGroupEntry {
+                        binding: 0,
+                        resource: wgpu::BindingResource::TextureView(&diffuse_texture_view),
+                    },
+                    wgpu::BindGroupEntry {
+                        binding: 1,
+                        resource: wgpu::BindingResource::Sampler(&diffuse_sampler),
+                    }
+                ],
+                label: Some("diffuse_bind_group"),
+            }
+        );
+
+
         let render_pipeline_layout =
             display.device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[&uniform_bind_group_layout],
+                bind_group_layouts: &[
+                    &texture_bind_group_layout,
+                    &uniform_bind_group_layout
+                ],
                 push_constant_ranges: &[],
             });
 
@@ -123,7 +229,7 @@ impl Game for JumpAndRun {
                     wgpu::VertexBufferLayout {
                         array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
                         step_mode: wgpu::InputStepMode::Vertex,
-                        attributes: &wgpu::vertex_attr_array![0 => Float2, 1 => Float3],
+                        attributes: &wgpu::vertex_attr_array![0 => Float2, 1 => Float2],
                     }
                 ],
             },
@@ -154,11 +260,11 @@ impl Game for JumpAndRun {
         });
 
         let vertices : Vec<Vertex> = vec![
-            Vertex { position: glam::vec2(-0.08682410,  0.49240386), color: glam::vec3(0.5, 0.0, 0.5) }, // A
-            Vertex { position: glam::vec2(-0.49513406,  0.06958647), color: glam::vec3(0.5, 0.2, 0.5) }, // B
-            Vertex { position: glam::vec2(-0.21918549, -0.44939706), color: glam::vec3(0.5, 0.4, 0.5) }, // C
-            Vertex { position: glam::vec2( 0.35966998, -0.34732910), color: glam::vec3(0.5, 0.6, 0.5) }, // D
-            Vertex { position: glam::vec2( 0.44147372,  0.23473590), color: glam::vec3(0.5, 0.8, 0.5) }, // E
+            Vertex { position: glam::vec2(-0.08682410,  0.49240386), tex_coords: glam::vec2(0.4131759000, 0.992403860) }, // A
+            Vertex { position: glam::vec2(-0.49513406,  0.06958647), tex_coords: glam::vec2(0.0048659444, 0.569586460) }, // B
+            Vertex { position: glam::vec2(-0.21918549, -0.44939706), tex_coords: glam::vec2(0.2808145300, 0.050602943) }, // C
+            Vertex { position: glam::vec2( 0.35966998, -0.34732910), tex_coords: glam::vec2(0.8596700000, 0.152670890) }, // D
+            Vertex { position: glam::vec2( 0.44147372,  0.23473590), tex_coords: glam::vec2(0.9414737000, 0.734735900) }, // E
         ];
 
         let vertex_buffer = display.device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
@@ -185,7 +291,8 @@ impl Game for JumpAndRun {
             num_indices,
             camera,
             uniform_buffer,
-            uniform_bind_group
+            uniform_bind_group,
+            diffuse_bind_group
         })
     }
 
@@ -240,7 +347,8 @@ impl Game for JumpAndRun {
         render_pass.set_pipeline(&self.render_pipeline);
         render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
         render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
-        render_pass.set_bind_group( 0, &self.uniform_bind_group, &[]);
+        render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
+        render_pass.set_bind_group(1, &self.uniform_bind_group, &[]);
         render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
 
         //Ok(())
--- src/shader.vert
+++ src/shader.vert
@@ -1,17 +1,17 @@
 #version 450
 
 layout(location=0) in vec2 a_position;
-layout(location=1) in vec3 a_color;
+layout(location=1) in vec2 a_tex_coords;
 
-layout(location=0) out vec3 v_color;
+layout(location=0) out vec2 v_tex_coords;
 
-layout(set=0, binding=0) // 1.
+layout(set=1, binding=0) // 1.
 uniform Uniforms {
     mat4 cam; // 2.
 };
 
 
 void main() {
-    v_color = a_color;
+    v_tex_coords = a_tex_coords;
     gl_Position = cam * vec4(a_position, 0.0, 1.0);
 }

e35dfc2696921a802ac911420e94b4a876034ddf
sidit77 <sidit77@gmail.com>
2021-03-24 11:52:17 +0100

    Changed texture filtering to linear

--- src/main.rs
+++ src/main.rs
@@ -91,8 +91,8 @@ impl Game for JumpAndRun {
             address_mode_u: wgpu::AddressMode::ClampToEdge,
             address_mode_v: wgpu::AddressMode::ClampToEdge,
             address_mode_w: wgpu::AddressMode::ClampToEdge,
-            mag_filter: wgpu::FilterMode::Nearest,
-            min_filter: wgpu::FilterMode::Nearest,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Linear,
             mipmap_filter: wgpu::FilterMode::Linear,
             ..Default::default()
         });
